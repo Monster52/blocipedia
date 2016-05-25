f1ad1503ab6713be7777d7fc50acffda85dcb237
Ross Waguespack <rosswaguespack@gmail.com>
2016-05-24 20:25:03 -0500

    Set up Devise for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set up Devise for production
+  config.action_mailer.default_url_options = { :host => 'sheltered-dawn-69711.heroku.com'}
 end
