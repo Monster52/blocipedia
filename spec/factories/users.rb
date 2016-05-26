9a0017c6ec5e066f3f26c0f242c4556626da0fd7
Ross Waguespack <rosswaguespack@gmail.com>
2016-05-25 22:50:54 -0500

    User attribute test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
+  pw = ('a'..'h').to_a.shuffle.join
   factory :user do
-    
+    username 'hack'
+    email 'hack@email.com'
+    password pw
+    password_confirmation pw
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "attributes" do
+    it { should have_db_column(:username).of_type(:string) }
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:encrypted_password).of_type(:string) }
+  end
 end
