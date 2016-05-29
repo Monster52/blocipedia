2125d3964e3adc6a89a6c75f663c7f3ce93b34b9
Ross Waguespack <rosswaguespack@gmail.com>
2016-05-29 09:50:54 -0500

    Create Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+    User.create!(
+      username: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'password',
+      confirmed_at: Time.now,
+      role: 'standard'
+    )
+end
+users = User.all
+puts "#{User.count} users created."
+
+# Create Admin
+unless User.find_by(email: "rosswaguespack@gmail.com")
+  admin = User.create!(
+    username: "Wagues23",
+    email: "rosswaguespack@gmail.com",
+    password: "password",
+    confirmed_at: Time.now,
+    role: 'admin'
+  )
+  puts "created Admin User."
+  puts "Email: #{admin.email} Password: #{admin.password}"
+else
+  puts "Skipped creation of \"rosswaguespack@gmail.com\""
+end
+
+# Create Wikis
+20.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph(3),
+    user:  users.sample
+  )
+end
+puts "#{Wiki.count} wikis created."
