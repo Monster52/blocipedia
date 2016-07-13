3634a0d011ad011c0485202c34afeaf8b8eb78dd
Xander Miller <xander.miller@gmail.com>
2016-07-13 02:46:31 +0000

    change migration take 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602013714) do
+ActiveRecord::Schema.define(version: 20160713023831) do
 
   create_table "collaborations", force: :cascade do |t|
     t.integer  "user_id"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20160602013714) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "role"
+    t.integer  "role",                                null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
