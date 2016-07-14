75a11d8a53836cb4ad2f3c10497e624aa117d45d
Ross Waguespack <rosswaguespack@gmail.com>
2016-07-13 23:39:06 -0500

    Add validations to wikis and collaborations

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe Wiki, type: :model do
 
   describe "associations" do
     it { should belong_to(:user) }
+    it { should have_many(:collaborations) }
+    it { should have_many(:users) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_least(5)}
+
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(20) }
+
+    it { should validate_presence_of(:user) }
   end
 
 end
